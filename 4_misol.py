dc033cd681fd5a06287f0934684ee0644e219b6c
Javohir Sadullayev <119050261+Javohir1106@users.noreply.github.com>
2024-10-22 19:11:12 +0500

    list misollar

---
+++ 4_misol.py
@@ -0,0 +1,9 @@
+"""Indeks bo'yicha elementlarni o'chirsh. Berilgan ro'yxatdagi 2-indeksdagi elementni o'chiring.
+cities=["New York", "London", "Tokyo", "Moscow", "Paris"]
+Natija: ["New York", "London", "Moscow", "Paris"]"""
+
+cities=["New York", "London", "Tokyo", "Moscow", "Paris"]
+
+cities.pop(2)
+
+print(cities)
